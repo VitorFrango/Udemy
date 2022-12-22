2d22bf962f1ea13cf88ecdd8e3a143da214cd7d7
Vitor Frango <51138494+VitorFrango@users.noreply.github.com>
2022-12-22 23:55:51 +0000

    Add files via upload

---
+++ S7Exer5.c
@@ -0,0 +1,28 @@
+//
+// Created by Vitor Frango on 22/12/2022.
+//
+
+#include <stdio.h>
+
+int main(){
+
+    int numeros[10];
+
+    for (int i = 0; i < 10; ++i) {
+        printf("Escreva 10 numeros\n");
+        scanf("%d", &numeros[i]);
+    }
+
+    for (int i = 0; i < 10 ; ++i) {
+        printf("O numeros introduzidos foram: [%d]\n", numeros[i]);
+
+    }
+
+    for (int i = 0; i < 10; ++i) {
+        if (i%0)
+
+
+    }
+
+    return 0;
+}
